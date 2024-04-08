2097d49be154a38d704b435c2103f35c82cd34bc
souvik <souvikde.ns@gmail.com>
2024-04-08 12:11:47 +0530

    feat:update main.go

--- main.go
+++ main.go
@@ -6,16 +6,11 @@ import (
 
 	"github.com/Souvikns/parser-go/parser"
 	model "github.com/Souvikns/parser-go/models/3.0.0"
-	"os"
 )
 
 func main() {
-	spec, err := os.ReadFile(("./spec.yaml"))
-	if err !=nil {
-		log.Fatalf(err.Error())
-	}
 	var asyncapi model.AsyncApi_3Dot_0Dot_0SchemaDot
-	err = parser.Parse(string(spec), &asyncapi)
+	err := parser.ParseFromFile("./spec.yaml", &asyncapi)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
