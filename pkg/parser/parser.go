931defd6fffffb412f801788323963e7f50dd66e
souvik <souvikde.ns@gmail.com>
2024-05-13 18:07:36 +0530

    feat: add support functions for parse func

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -3,6 +3,9 @@ package parser
 import (
 	"encoding/json"
 	"errors"
+	"io"
+	"net/http"
+	"os"
 	parserError "github.com/Souvikns/parser-go/pkg/error"
 	schemes "github.com/asyncapi/spec-json-schemas/v6"
 	"github.com/xeipuuv/gojsonschema"
@@ -44,6 +47,30 @@ func Parse[K Asyncapi_3_0_0 | Asyncapi_2_6_0 | Asyncapi_2_5_0 | Asyncapi_2_4_0 |
 
 	return nil
 
+}
+
+func ParseFromFile[K Asyncapi_3_0_0 | Asyncapi_2_6_0 | Asyncapi_2_5_0 | Asyncapi_2_4_0 | Asyncapi_2_3_0 | Asyncapi_2_2_0 | Asyncapi_2_1_0 | Asyncapi_2_0_0](filepath string, obj *K) error {
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		return err
+	}
+
+	return Parse(string(data), obj)
+}
+
+func ParseFromUrl[K Asyncapi_3_0_0 | Asyncapi_2_6_0 | Asyncapi_2_5_0 | Asyncapi_2_4_0 | Asyncapi_2_3_0 | Asyncapi_2_2_0 | Asyncapi_2_1_0 | Asyncapi_2_0_0](url string, obj *K) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	return Parse(string(body), obj)
 }
 
 func loadDocument(document []byte) (Document, error) {
